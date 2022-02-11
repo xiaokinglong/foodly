32a4b4db5950163134201aebca06009e6cb0ce4c
xiaokinglong <xiaokinglong@gmail.com>
2022-02-12 03:06:44 +0800

    使用了sliver

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -11,87 +11,90 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {},
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            title: Text("Home"),
           ),
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {},
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (content, index) {
+                return const HomeListItem();
+              },
+              childCount: 10,
+            ),
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: 50,
-        // itemExtent: 50.0,
-        itemBuilder: (BuildContext context, int index) {
-          // 卡片组件
-          return Container(
-            height: 307.0,
-            padding: const EdgeInsets.fromLTRB(18, 14, 18, 14),
-            child: Container(
-                height: 279.0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+}
+
+class HomeListItem extends StatelessWidget {
+  const HomeListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 307.0,
+      padding: const EdgeInsets.fromLTRB(18, 14, 18, 14),
+      child: Container(
+          height: 279.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 134,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://img1.baidu.com/it/u=1897395871,1393698560&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=685",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                child: const Text("Carl's Jr. (1001 Veterans Blvd)"),
+              ),
+              Container(
+                child: const Text("\$\$ . Fast Food . Burger . Chicken"),
+              ),
+              Container(
+                child: Row(
                   children: [
-                    Container(
-                      height: 134,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                        color: Colors.yellow,
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            "https://img1.baidu.com/it/u=1897395871,1393698560&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=685",
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                    RaisedButton(
+                      onPressed: () {
+                        print("onPressed");
+                      },
+                      onLongPress: () {
+                        print("onLongPress");
+                      },
+                      child: Text("3.4 mi"),
+                    ),
+                    Text("button"),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      color: Colors.black,
+                      icon: const Icon(Icons.wechat),
                     ),
                     Container(
-                      child: const Text("Carl's Jr. (1001 Veterans Blvd)"),
-                    ),
-                    Container(
-                      child: const Text("\$\$ . Fast Food . Burger . Chicken"),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          RaisedButton(
-                            onPressed: () {
-                              print("onPressed");
-                            },
-                            onLongPress: () {
-                              print("onLongPress");
-                            },
-                            child: Text("3.4 mi"),
-                          ),
-                          Text("button"),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            color: Colors.black,
-                            icon: const Icon(Icons.wechat),
-                          ),
-                          Container(
-                            child: const Text("People say : well packed (15)"),
-                          ),
-                        ],
-                      ),
+                      child: const Text("People say : well packed (15)"),
                     ),
                   ],
-                )),
-          );
-        },
-      ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
